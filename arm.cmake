c80de7975eead5cfd5b98703974339fff9f42aab
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2018-01-21 10:14:36 -0700

    cmake updates

--- arm.cmake
+++ arm.cmake
@@ -1,16 +1,14 @@
 
 
 #Add sources to the project
-set(SOURCES_PREFIX ${CMAKE_SOURCE_DIR}/src CACHE INTERNAL "src prefix")
+set(SOURCES_PREFIX ${CMAKE_SOURCE_DIR}/src)
 add_subdirectory(src)
 list(APPEND SOS_LIB_SOURCELIST ${SOURCES})
 
-
-set(SOS_LIB_ARCH armv7-m CACHE INTERNAL "sos build ")
-
-set(SOS_LIB_CONFIG release CACHE INTERNAL "sos build config release")
-set(SOS_LIB_BUILD_FLAGS -mlong-calls CACHE INTERNAL "sos lib build flags")
+set(SOS_LIB_OPTION "")
+set(SOS_LIB_TYPE release)
+set(SOS_LIB_BUILD_FLAGS -mlong-calls)
+set(SOS_LIB_ARCH armv7-m)
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib.cmake)
-
-set(SOS_LIB_ARCH armv7e-m CACHE INTERNAL "sos build ")
+set(SOS_LIB_ARCH armv7e-m)
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib.cmake)
--- link.cmake
+++ link.cmake
@@ -1,10 +1,11 @@
 
 
 #Add sources to the project
-set(SOURCES_PREFIX ${CMAKE_SOURCE_DIR}/src CACHE INTERNAL "src prefix")
+set(SOURCES_PREFIX ${CMAKE_SOURCE_DIR}/src)
 add_subdirectory(src)
 list(APPEND SOS_LIB_SOURCELIST ${SOURCES})
 
-set(SOS_LIB_CONFIG release CACHE INTERNAL "sos build config release")
-set(SOS_LIB_ARCH link CACHE INTERNAL "sos build ")
+set(SOS_LIB_OPTION "")
+set(SOS_LIB_TYPE release)
+set(SOS_LIB_ARCH link)
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib.cmake)
