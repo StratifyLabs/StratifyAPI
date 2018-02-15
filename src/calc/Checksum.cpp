bc254e36ace5d4722ed2a0499f5bd05f46a0cbef
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2018-02-14 17:25:26 -0700

    Added checksum calculations

---
+++ src/calc/Checksum.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+#include "calc/Checksum.hpp"
+
+using namespace calc;
+
+u8 Checksum::calc_zero_sum(const u8 * data, int size){
+    int i;
+    u8 sum = 0;
+    int count = size/sizeof(u8) - 1;
+    for(i=0; i < count; i++){
+        sum += data[i];
+    }
+
+    return (0 - sum);
+}
+
+bool Checksum::verify_zero_sum(const u8 * data, int size){
+    int i;
+    u8 sum = 0;
+    int count = size/sizeof(u8);
+    for(i=0; i < count; i++){
+        printf("0x%X ", data[i]);
+        sum += data[i];
+    }
+
+    printf("\n");
+    return (sum == 0);
+}
+
+
+u32 Checksum::calc_zero_sum(const u32 * data, int size){
+    int i;
+    u8 sum = 0;
+    int count = size/sizeof(u32) - 1;
+    for(i=0; i < count; i++){
+        sum += data[i];
+    }
+
+    return (0 - sum);
+}
+
+bool Checksum::verify_zero_sum(const u32 * data, int size){
+    int i;
+    u8 sum = 0;
+    int count = size/sizeof(u32);
+    for(i=0; i < count; i++){
+        sum += data[i];
+    }
+
+    return sum == 0;
+}
