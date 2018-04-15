d7f9bd021f4e850a4381f9918366d43cefbc1974
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2018-04-15 16:13:51 -0600

    Bug fix and docs on var::Array

--- include/var/Array.hpp
+++ include/var/Array.hpp
@@ -5,19 +5,46 @@
 
 namespace var {
 
+/*! \brief Array Class
+ * \details The Array class is a template
+ * used to contain a standard C array.
+ *
+ * It provides methods to access the array
+ * safely.
+ *
+ */
 template <typename T, u32 size_value> class Array : public api::SObject {
 public:
+
     Array(){}
 
+    /*! \details Returns the number of objects in the array. */
+    int count() const { return size_value; }
     int size() const { return size_value; }
 
+    /*! \details Returns the first object in the array. */
     T front(){ return m_array[0]; }
+    /*! \details Returns the first object in the array (read-only). */
     const T & front() const { return m_array[0]; }
 
-    T back(){ return m_array[size_value-1]; }
-    const T & back() const { return m_array[size_value-1]; }
+    /*! \details Returns the last object in the array. */
+    T back(){
+        if( size_value ){
+            return m_array[size_value-1];
+        } else {
+            return m_array[0];
+        }
+    }
+    /*! \details Returns the last object in the array (read-only). */
+    const T & back() const {
+        if( size_value ){
+            return m_array[size_value-1];
+        } else {
+            return m_array[0];
+        }
+    }
 
-
+    /*! \details Fills the array with the specified \a value. */
     void fill(const T & value){
         u32 i;
         for(i=0; i < size(); i++){
@@ -25,14 +52,29 @@ public:
         }
     }
 
+    /*! \details Returns a read-only item of the index value specified.
+     *
+     * @param idx The object index
+     *
+     * If the idx exceeds the size of the array, SObject::exit_fatal()
+     * is called.
+     *
+     */
     const T & at(u32 idx) const {
-        if( idx < size_value ){
-            return m_array[idx];
+        if( idx >= size_value ){
+            exit_fatal("Array out of bounds");
         }
-        //fatal error
-        return 0;
+        return m_array[idx];
     }
 
+    /*! \details Returns an item of the index value specified.
+     *
+     * @param idx The object index
+     *
+     * If the idx exceeds the size of the array, SObject::exit_fatal()
+     * is called.
+     *
+     */
     T & at(u32 idx){
         if( idx < size_value ){
             return m_array[idx];
@@ -41,7 +83,10 @@ public:
         return 0;
     }
 
+    /*! \details Returns a pointer to the data. */
     T * data(){ return m_array; }
+
+    /*! \details Returns a read-only pointer to the data. */
     const T * data() const { return m_array; }
 
 private:
