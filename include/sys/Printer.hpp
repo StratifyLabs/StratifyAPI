dda81ab520641067ea6d3be821031fbf05fd4599
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-02-15 08:31:43 -0700

    Doc updates

--- include/sys/Printer.hpp
+++ include/sys/Printer.hpp
@@ -67,6 +67,28 @@ public:
 private:
 };
 
+
+/*! \brief Printer Class
+ * \details The Printer class is used for printing
+ * YAML formatted messages to the standard output.
+ * The printing mechanisms are virtual and can
+ * be re-implemented to print to any output
+ * (or multiple outputs).
+ *
+ * The Printer class supports the `<<` operator
+ * for a growing list of StratifyAPI types.
+ *
+ * For example, the following code will
+ * print the system information as a
+ * YAML object.
+ *
+ * ```
+ * #include <sapi/sys.hpp>
+ * Printer p;
+ * p.open_object("System Information") << SysInfo::get() << p.close();
+ * ```
+ *
+ */
 class Printer : public api::SysWorkObject {
 public:
 	Printer();
@@ -97,92 +119,159 @@ public:
 	static unsigned int m_default_color;
 #endif
 
+	/*! \details Prints a var::Data object. */
 	Printer & operator << (const var::Data & a);
+	/*! \details Prints a var::DataInfo object. */
 	Printer & operator << (const var::DataInfo & a);
+	/*! \details Prints a var::String object. */
 	Printer & operator << (const var::String & a);
+	/*! \details Prints a var::Tokenizer object. */
 	Printer & operator << (const var::Tokenizer & a);
+	/*! \details Prints a var::JsonObject object. */
 	Printer & operator << (const var::JsonObject & a);
+	/*! \details Prints a var::JsonArray object. */
 	Printer & operator << (const var::JsonArray & a);
+	/*! \details Prints a string list object. */
 	Printer & operator << (const var::Vector<var::String> & a);
+	/*! \details Prints a var::Ring<u32> object. */
 	Printer & operator << (const var::Ring<u32> & a);
+	/*! \details Prints a var::Ring<s32> object. */
 	Printer & operator << (const var::Ring<s32> & a);
+	/*! \details Prints a var::Ring<u16> object. */
 	Printer & operator << (const var::Ring<u16> & a);
+	/*! \details Prints a var::Ring<s16> object. */
 	Printer & operator << (const var::Ring<s16> & a);
+	/*! \details Prints a var::Ring<u8> object. */
 	Printer & operator << (const var::Ring<u8> & a);
+	/*! \details Prints a var::Ring<s8> object. */
 	Printer & operator << (const var::Ring<s8> & a);
+
+	/*! \details Prints a sys::Cli object. */
 	Printer & operator << (const Cli & a);
+	/*! \details Prints a sys::appfs_file_t object. */
 	Printer & operator << (const appfs_file_t & a);
+	/*! \details Prints a sys::AppfsFileAttributes object. */
 	Printer & operator << (const AppfsFileAttributes & a);
+	/*! \details Prints a sys::TraceEvent object. */
 	Printer & operator << (const TraceEvent & a);
 
+	/*! \details Prints a sys::SysInfo object. */
+	Printer & operator << (const sys::SysInfo & a);
+	/*! \details Prints a sys::AppfsInfo object. */
+	Printer & operator << (const sys::AppfsInfo & a);
+	/*! \details Prints a sys::TaskInfo object. */
+	Printer & operator << (const sys::TaskInfo & a);
+	/*! \details Prints a sys::FileInfo object. */
+	Printer & operator << (const sys::FileInfo & a);
+
+	/*! \details Prints a sgfx::Bitmap object. */
 	Printer & operator << (const sgfx::Bitmap & a);
+	/*! \details Prints a sgfx::Cursor object. */
 	Printer & operator << (const sgfx::Cursor & a);
+	/*! \details Prints a sgfx::Point object. */
 	Printer & operator << (const sgfx::Point & a);
+	/*! \details Prints a sgfx::Region object. */
 	Printer & operator << (const sgfx::Region & a);
+	/*! \details Prints a sgfx::Area object. */
 	Printer & operator << (const sgfx::Area & a);
+	/*! \details Prints a sgfx::Pen object. */
 	Printer & operator << (const sgfx::Pen & a);
+	/*! \details Prints a sgfx::Vector object. */
 	Printer & operator << (const sgfx::Vector & a);
+	/*! \details Prints a sgfx::VectorPath object. */
 	Printer & operator << (const sgfx::VectorPath & a);
+	/*! \details Prints a sgfx::VectorPathDescription object. */
 	Printer & operator << (const sgfx::VectorPathDescription & a);
 
 
-
-	Printer & operator << (const sys::SysInfo & a);
-	Printer & operator << (const sys::AppfsInfo & a);
-	Printer & operator << (const sys::TaskInfo & a);
-	Printer & operator << (const sys::FileInfo & a);
-
+	/*! \details Prints a chrono::ClockTime object. */
 	Printer & operator << (const chrono::ClockTime & a);
+	/*! \details Prints a chrono::MicroTime object. */
 	Printer & operator << (const chrono::MicroTime & a);
+	/*! \details Prints a chrono::Time object. */
 	Printer & operator << (const chrono::Time & a);
+
+	/*! \details Prints a basic s32 type (see set_flags() for details). */
 	Printer & operator << (s32 a);
+	/*! \details Prints a basic u32 type (see set_flags() for details). */
 	Printer & operator << (u32 a);
+	/*! \details Prints a basic s16 type (see set_flags() for details). */
 	Printer & operator << (s16 a);
+	/*! \details Prints a basic u16 type (see set_flags() for details). */
 	Printer & operator << (u16 a);
+	/*! \details Prints a basic s8 type (see set_flags() for details). */
 	Printer & operator << (s8 a);
+	/*! \details Prints a basic u8 type (see set_flags() for details). */
 	Printer & operator << (u8 a);
+	/*! \details Prints a pointer. */
 	Printer & operator << (void * a);
+	/*! \details Prints a c-style string. */
 	Printer & operator << (const char * a);
+	/*! \details Prints a float type. */
 	Printer & operator << (float a);
 
+	/*! \details Filtering levels. */
 	enum verbose_level {
-		FATAL,
-		ERROR,
-		WARNING,
-		INFO,
-		MESSAGE,
-		DEBUG
+		FATAL /*! Prints fatal errors. */,
+		ERROR /*! Prints fatal and error messages. */,
+		WARNING /*! Printers warnings and worse. */,
+		INFO /*! Prints basic info (default setting) plus warning and errors. */,
+		MESSAGE /*! Prints additional messages. */,
+		DEBUG /*! Prints everything. */
 	};
 
+	/*! \details Assign an effective verbose level to this object. */
 	void set_verbose_level(enum verbose_level level){
 		m_verbose_level = level;
 	}
 
+	/*! \details Assign a verbose level.
+	 *
+	 * @param level The level as 'fatal', 'error', 'warning', 'info', 'message', or 'debug'
+	 * @return Zero on success
+	 */
 	int set_verbose_level(const var::ConstString & level);
 
+	/*! \details Returns the current verbose level. */
 	enum verbose_level verbose_level() const { return m_verbose_level; }
 
+	/*! \details Prints a debug message if the verbose level is set to debug. */
 	virtual Printer & debug(const char * fmt, ...);
+	/*! \details Prints a message (filtered according to verbose_level()). */
 	virtual Printer & message(const char * fmt, ...);
+	/*! \details Prints information (filtered according to verbose_level()). */
 	virtual Printer & info(const char * fmt, ...);
+	/*! \details Prints a warning (filtered according to verbose_level()). */
 	virtual Printer & warning(const char * fmt, ...);
+	/*! \details Prints an error (filtered according to verbose_level()). */
 	virtual Printer & error(const char * fmt, ...);
+	/*! \details Prints a fatal error message. */
 	virtual Printer & fatal(const char * fmt, ...);
 
+	/*! \details Prints a debug message if the verbose level is set to debug. */
 	Printer & debug(const var::ConstString & a){ return debug(a.cstring()); }
+	/*! \details Prints a message (filtered according to verbose_level()). */
 	Printer & message(const var::ConstString & a){ return message(a.cstring()); }
+	/*! \details Prints information (filtered according to verbose_level()). */
 	Printer & info(const var::ConstString & a){ return info(a.cstring()); }
+	/*! \details Prints a warning (filtered according to verbose_level()). */
 	Printer & warning(const var::ConstString & a){ return warning(a.cstring()); }
+	/*! \details Prints an error (filtered according to verbose_level()). */
 	Printer & error(const var::ConstString & a){ return error(a.cstring()); }
+	/*! \details Prints a fatal error message. */
 	Printer & fatal(const var::ConstString & a){ return fatal(a.cstring()); }
 
+	/*! \details Prints a key value pair. */
 	virtual Printer & key(const var::ConstString & key, const char * fmt, ...);
+	/*! \details Prints a key value pair. */
 	virtual Printer & key(const var::ConstString & key, const var::String & a);
+	/*! \details Prints a key value pair. */
 	virtual Printer & key(const var::ConstString & key, const var::JsonValue & a);
 
 	void print(const char * fmt, ...);
 
 
+	/*! \details Number printing flags. */
 	enum {
 		PRINT_HEX /*! Print hex data */ = (1<<0),
 		PRINT_UNSIGNED /*! Print unsigned integers */ = (1<<1),
@@ -194,8 +283,32 @@ public:
 		PRINT_SIMPLE_PROGRESS /*! Just print # for progress */ = (1<<6)
 	};
 
+	/*! \details Sets the flags that modify how numbers and some messages are printed. */
 	void set_flags(u32 value){ m_o_flags = value; }
 
+	/*! \details Opens a YAML object.
+	 *
+	 * @param key The key to be used for the object
+	 * @param level The filter level for child keys
+	 *
+	 * YAML object values are not preceded by '-' while arrays are.
+	 * Objects should have unique keys in order to have properly
+	 * formatted YAML. The debug(), info(), warning(), error(),
+	 * and fatal() methods can be used at most once inside of an object
+	 * without having a duplicate key. The key() method can be used to assign
+	 * unique keys to each object.
+	 *
+	 * ```
+	 * Printer p;
+	 * //whole object only prints with debug verbose level
+	 * p.open_object("keys", Printer::DEBUG);
+	 * p.key("key0", "value0");
+	 * p.key("key1", "value1");
+	 * p.key("key2", "value2");
+	 * p.close_object();
+	 * ```
+	 *
+	 */
 	Printer & open_object(const var::ConstString & key, enum verbose_level level = FATAL);
 	void close_object(){
 		m_container.pop_back();
@@ -204,11 +317,59 @@ public:
 		}
 	}
 
+	/*! \details Opens a YAML array.
+	 *
+	 * @param key The key to be used for the object
+	 * @param level The filter level for child keys
+	 *
+	 * YAML object values are not preceded by '-' while arrays are.
+	 * Objects should have unique keys in order to have properly
+	 * formatted YAML. The debug(), info(), warning(), error(),
+	 * and fatal() methods can be used multple times inside of
+	 * an array and are indexed when parsed.
+	 *
+	 *
+	 *
+	 * ```
+	 * Printer p;
+	 * //whole object only prints with debug verbose level
+	 * p.open_array("keys", Printer::MESSAGE);
+	 * p.info("Hello");
+	 * p.info("Hello");
+	 * p.info("Hello");
+	 * p.warning("Hello");
+	 * p.message("Hello");
+	 * p.message("Hello");
+	 * p.warning("Hello");
+	 * p.close_array(); //close_array() and close_object() are interchangeable
+	 * ```
+	 *
+	 *
+	 * By default the Printer starts with an open array.  The following
+	 * code can change the top level type to an object.
+	 *
+	 * ```
+	 * Printer p; //printer is now an array
+	 * p.close_array();
+	 * p.open_object(); //top level type is an object
+	 * ```
+	 *
+	 */
 	Printer & open_array(const var::ConstString & key, enum verbose_level level = FATAL);
 	void close_array(){
 		close_object();
 	}
 
+	/*! \details Returns a printer termination object for closing
+	 * an array or object with the `<<` operator.
+	 *
+	 * ```
+	 * Printer p;
+	 * p.open_object("empty object") << p.close();
+	 * p.open_array("messages", Printer::MESSAGE) << p.message("hello") << p.close();
+	 * ```
+	 *
+	 */
 	static PrinterTermination close(){
 		return PrinterTermination();
 	}
@@ -218,29 +379,72 @@ public:
 		return *this;
 	}
 
-
+	/*! \details Returns a pointer to the sys::ProgressCallback member.
+	 *
+	 * This method can be used for operations that use sys::ProgressCallback
+	 * class to update the progress. The progess is printed to the screen
+	 * with the key set to progress_key().
+	 *
+	 * ```
+	 * #include <sapi/sys.hpp>
+	 * #include <sapi/inet.hpp>
+	 * Printer p;
+	 *
+	 * p.set_progress_key("downloading");
+	 * HttpClient http_client;
+	 * DataFile data_file(File::APPEND);
+	 *
+	 * //download file to data_file and print the progress using #'s
+	 * http_client.get("http://some.url/file", data_file, p.progress_callback());
+	 * ```
+	 *
+	 */
 	const sys::ProgressCallback * progress_callback() const {
 		return &m_progress_callback;
 	}
 
+	/*! \details Used by progres_callback(). */
 	static bool update_progress_callback(void * context, int progress, int total){
 		Printer * printer = (Printer*)context;
 		return printer->update_progress(progress, total);
 	}
 
+	/*! \details Used by progres_callback(). */
 	bool update_progress(int progress, int total);
 
+	/*! \details Access to the key to print during progress updates. */
 	var::String & progress_key(){ return m_progress_key; }
+	/*! \details Access (read-only) to the key to print during progress updates. */
 	const var::String & progress_key() const { return m_progress_key; }
 
+	 /*! \details Returns a character that will be printed.
+	 *
+	 * @param color The color of the pixel
+	 * @param bits_per_pixel The number of bits per pixel in the color
+	 * @return A character (like '*') to be used for the color
+	 *
+	 * This method is used for printing 1, 2, and 4, bit per pixel
+	 * colors.
+	 *
+	 */
 	static char get_bitmap_pixel_character(u32 color, u8 bits_per_pixel);
 
 protected:
 
+	/*! \details Prints the YAML indentation. */
 	void print_indentation();
 
+	/*! \details Prints an indented key (can be re-implemented). */
 	virtual void print_indented(const var::ConstString & key, const char * fmt, ...);
+	/*! \details Prints an indented key (can be re-implemented). */
 	virtual void vprint_indented(const var::ConstString & key, const char * fmt, va_list list);
+	/*! \details Prints all characters.
+	 *
+	 * This is the only method that needs to be re-implemented
+	 * to re-direct the Printer from the standard output to
+	 * another file or output.
+	 *
+	 */
 	virtual void vprint(const char * fmt, va_list list);
 
 private:
--- include/sys/ProgressCallback.hpp
+++ include/sys/ProgressCallback.hpp
@@ -6,20 +6,67 @@
 
 namespace sys {
 
+/*! \brief ProgressCallback Class
+ * \details The ProgressCallback class is used
+ * by various methods to allow the application
+ * to update the progress of an operation.
+ *
+ * ```
+ * #include <sapi/sys.hpp>
+ * #include <sapi/inet.hpp>
+ * Printer p;
+ *
+ * p.set_progress_key("downloading");
+ * HttpClient http_client;
+ * DataFile data_file(File::APPEND);
+ *
+ * //download file to data_file and print the progress using #'s
+ * http_client.get("http://some.url/file", data_file, p.progress_callback());
+ * ```
+ *
+ */
 class ProgressCallback : public api::SysInfoObject {
 public:
+
+	/*! \details Defines the callback function prototype.
+	 *
+	 * The function has three arguments.  The first
+	 * is the context() member of this object. The first
+	 * integer is the current progress value and the second
+	 * is the total progress value.
+	 *
+	 * If the total (third argument) is zero, the operation is either complete
+	 * or aborted.
+	 *
+	 */
 	typedef bool (*callback_t)(void*, int, int);
 
+	/*! \details Constructs an empty object. */
 	ProgressCallback();
+
+	/*! \details Constructs a fully initialized callback.
+	 *
+	 * @param callback The function to execute
+	 * @param context The context (first argument passed to callback)
+	 *
+	 */
 	ProgressCallback(callback_t callback, void * context = 0);
 
 
+	/*! \details Assigns a callback. */
 	void set_callback(bool (*callback)(void*,int,int)){
 		m_update = callback;
 	}
 
+	/*! \details Assigns a context. */
 	void set_context(void * context){ m_context = context; }
 
+	/*! \details Executes the callback if it is valid.
+	 *
+	 * This method is called within operations in order
+	 * to execute the provided callback.
+	 *
+	 */
 	bool update(int value, int total) const;
 
 private:
