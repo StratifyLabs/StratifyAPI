3cc500842633bd4700eabcd9f5eaaf39e1afbd89
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-03-26 13:05:35 -0600

    build link error fixed

--- src/api/DspObject.cpp
+++ src/api/DspObject.cpp
@@ -4,10 +4,11 @@
 
 using namespace api;
 
+#if !defined __link
 DspQ7Api DspWorkObject::m_api_q7;
 DspQ15Api DspWorkObject::m_api_q15;
 DspQ31Api DspWorkObject::m_api_q31;
 DspF32Api DspWorkObject::m_api_f32;
 DspConversionApi DspWorkObject::m_api_conversion;
-
+#endif
 
