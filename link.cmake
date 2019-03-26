1b970745de5facaca506d632796355e584157655
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-03-26 12:28:40 -0600

    cmake variables update

--- link.cmake
+++ link.cmake
@@ -4,9 +4,9 @@
 set(SOURCES_PREFIX ${CMAKE_SOURCE_DIR}/src)
 add_subdirectory(src)
 file(GLOB_RECURSE HEADER_SOURCES ${CMAKE_SOURCE_DIR}/include/*)
-list(APPEND SOS_LIB_SOURCELIST ${SOURCES} ${HEADER_SOURCES})
+list(APPEND SOS_SOURCELIST ${SOURCES} ${HEADER_SOURCES})
 
-set(SOS_LIB_OPTION "")
-set(SOS_LIB_TYPE release)
-set(SOS_LIB_ARCH link)
+set(SOS_OPTION "")
+set(SOS_CONFIG release)
+set(SOS_ARCH link)
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib.cmake)
