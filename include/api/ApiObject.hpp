28d06500371e72064a214964fb071b6ffc38ee47
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-05-08 11:45:15 -0600

    version roll

--- include/api/ApiObject.hpp
+++ include/api/ApiObject.hpp
@@ -283,7 +283,7 @@ public:
 	 * ```
 		*
 		*/
-	static const char * version(){ return "3.16.0"; }
+	static const char * version(){ return "3.17.0"; }
 
 	/*! \details Returns a c-style string pointer
 	 * to the git hash used to build the Stratify API.
