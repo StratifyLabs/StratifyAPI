3d901cc08cff99e3011fe8a17fa6e15d9037883d
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-08-10 08:19:33 -0600

    minor bug fixes

--- src/var/String.cpp
+++ src/var/String.cpp
@@ -184,7 +184,7 @@ Vector<String> String::split(const String & delimiter) const {
 
 
 size_t String::count(const var::String & occurance) const {
-	u32 pos = 0;
+	size_t pos = 0;
 	u32 item_count = 0;
 	while( pos < length() && pos != String::npos){
 		pos = find(occurance, String::Position(pos));
