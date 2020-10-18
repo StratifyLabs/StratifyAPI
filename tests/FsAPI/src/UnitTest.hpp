8413baf71eca1741d6ce3ccb663938be7fb1591e
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-10-17 19:49:10 -0600

    Update UnitTest.hpp

--- tests/FsAPI/src/UnitTest.hpp
+++ tests/FsAPI/src/UnitTest.hpp
@@ -191,9 +191,7 @@ public:
   bool path_api_case() {
     printer::PrinterObject po(printer(), "path");
     TEST_ASSERT(P("data/test.json").path() == "data/test.json");
-    P name("flat");
-    TEST_EXPECT(name.path() == "flat");
-    TEST_ASSERT("flat" == name.name());
+    TEST_ASSERT(P("flat").name() == "flat");
     TEST_ASSERT(P("flat.json").name() == "flat.json");
     TEST_ASSERT(P("flat").suffix() == "");
     TEST_ASSERT(P("flat.json").suffix() == "json");
